3e18824c72b2162e2e0123eebf2b119cbd1afb79
irakli00 <iraklitsurtsumia@gmail.com>
2024-10-15 20:47:57 +0400

    fuck kvirikashvili

---
+++ script/db.js
@@ -0,0 +1,181 @@
+export const dogs =[
+  {
+  id:'MO231',
+  imgUrl:'../script/db_dog_imgs/pomerenianwhite.png',
+  breed:'Pomeranian White',
+  ageMonths:'02',
+  gene:'Male',
+  price:6900000,
+  currency:'VND'},
+  {
+  id:'MO502',
+  imgUrl:'../script/db_dog_imgs/poodlewhite.png',
+  breed:'Poodle Tiny Yellow',
+  ageMonths:'02',
+  gene:'Female',
+  price:3900000,
+  currency:'VND'},
+  {
+  id:'MO102',
+  imgUrl:'../script/db_dog_imgs/poodleseoia.png',
+  breed:'Poodle Tiny Sepia',
+  ageMonths:'02',
+  gene:'Male',
+  price:4000000,
+  currency:'VND'},
+  {
+  id:'MO512',
+  imgUrl:'../script/db_dog_imgs/malamute.png',
+  breed:'Alaskan Malamute Grey',
+  ageMonths:'02',
+  gene:'Male',
+  price:8900000,
+  currency:'VND'},
+  {
+  id:'MO231',
+  imgUrl:'../script/db_dog_imgs/pembrokecorgicream.png',
+  breed:'Pembroke Corgi Cream',
+  ageMonths:'02',
+  gene:'Male',
+  price:7900000,
+  currency:'VND'},
+  {
+  id:'MO502',
+  imgUrl:'../script/db_dog_imgs/pembrokecorgi.png',
+  breed:'Pembroke Corgi Tricolor',
+  ageMonths:'02',
+  gene:'Female',
+  price:9000000,
+  currency:'VND'},
+  {
+    id:'MO231',
+    imgUrl:'../script/db_dog_imgs/pomerenianwhite-2.png',
+    breed:'Pomeranian White',
+    ageMonths:'02',
+    gene:'Female',
+    price:6500000,
+    currency:'VND'},
+  {
+  id:'MO512',
+  imgUrl:'../script/db_dog_imgs/pomerenianwhite.png',
+  breed:'Poodle Tiny Dairy Cow',
+  ageMonths:'02',
+  gene:'Male',
+  price:5000000,
+  currency:'VND'},
+]
+
+export const products=[
+{
+  desc:'Reflex Plus Adult Cat Food Salmon',
+  imgUrl:'../script/db_product_imgs/reflexsalmon.png',
+  product:'Dog Food',
+  sizeGrams:385,
+  price:140000,
+  currency:'VND',
+  bonus:{
+    value: 'Free Toy & Free Shaker'
+  }
+},
+{
+  desc:'Reflex Plus Adult Cat Food Salmon',
+  imgUrl:'../script/db_product_imgs/reflex-salmon-2.png',
+  product:'Cat Food',
+  sizeGrams:1500,
+  price:165000,
+  currency:'VND',
+  bonus:{
+    value: 'Free Toy & Free Shaker'
+  }
+},
+{
+  desc:'Cat scratching ball toy kitten sisal rope ball',
+  imgUrl:'../script/db_product_imgs/scratcher.png',
+  product:'Toy',
+  sizeGrams:null,
+  price:1100000,
+  currency:'VND',
+  bonus:{
+    value: 'Free Cat Food'
+  }
+},
+{
+  desc:'Cute Pet Cat Warm Nest',
+  imgUrl:'../script/db_product_imgs/nest.png',
+  product:'Toy',
+  sizeGrams:null,
+  price:410000,
+  currency:'VND',
+  bonus:{
+    value: 'Free Cat Food'
+  }
+},
+{
+  desc:'NaturVet Dogs - Omega-Gold Plus Salmon Oil',
+  imgUrl:'../script/db_product_imgs/naturvet.png',
+  product:'Dog Food',
+  sizeGrams:385,
+  price:350000,
+  currency:'VND',
+  bonus:{
+    value: 'Free Toy & Free Shaker'
+  }
+},
+{
+  desc:'Costumes Fashion Pet Clother Cowboy Rider',
+  imgUrl:'../script/db_product_imgs/cowboydog.png',
+  product:'Costume',
+  sizeGrams:1500,
+  price:500000,
+  currency:'VND',
+  bonus:{
+    value: 'Free Toy & Free Shaker'
+  }
+},
+{
+  desc:'Costumes Chicken Drumsti ck Headband',
+  imgUrl:'../script/db_product_imgs/catchicken.png',
+  product:'Costume',
+  sizeGrams:null,
+  price:400000,
+  currency:'VND',
+  bonus:{
+    value: 'Free Cat Food'
+  }
+},
+{
+  desc:'Plush Pet Toy',
+  imgUrl:'../script/db_product_imgs/squid.png',
+  product:'Toy',
+  sizeGrams:null,
+  price:250000,
+  currency:'VND',
+  bonus:{
+    value: 'Free Food & Shaker'
+  }
+},
+]
+
+export const petFacts = [
+  {
+    imgUrl:'../imgs/facts/pomeranian.png',
+    header:'What is a Pomeranian? How to Identify Pomeranian Dogs',
+    fact:'The Pomeranian, also known as the Pomeranian (Pom dog), is always in the top of the cutest pets. Not only that, the small, lovely, smart, friendly, and skillful circus dog breed.',
+  },
+  {
+    imgUrl:'../imgs/facts/diet.png',
+    header:'Dog Diet You Need To Know',
+    fact:"Dividing a dog's diet may seem simple at first, but there are some rules you should know so that your dog can easily absorb the nutrients in the diet. For those who are just starting to raise dogs, especially newborn puppies with relatively weak resistance.",
+  },
+    {
+    imgUrl:'../imgs/facts/terminator.png',
+    header:'Why Dogs Bite and Destroy Furniture and How to Prevent It Effectively',
+    fact:'Dog bites are common during development. However, no one wants to see their furniture or important items being bitten by a dog.',
+  },  {
+    imgUrl:'../imgs/facts/diet.png',
+    header:'GENERATED',
+    fact:'sefjdlsk;fdj;sgfkjsdf;lkgdns;lg;dsgkmsklfgd;gmnfskjbgcmz.gnbjfsk;xzngjdfsk;znv/l',
+  },
+]
+
+export const eventDate = new Date(2024, 9, 20)
---
+++ script/script.js
@@ -0,0 +1,101 @@
+'use strict'
+
+import {dogs, products,petFacts, eventDate} from '../script/db.js'
+
+(function countdownTo(targetDate) {
+  const target = new Date(targetDate).getTime(); // Get the target date in milliseconds
+
+  const interval = setInterval(() => {
+    const now = new Date().getTime(); // Current time in milliseconds
+    const difference = target - now; // Difference between the target and now
+
+    // Calculate remaining time
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    // Display the result in the format: "D days HH:MM:SS"
+    document.getElementById("days").innerHTML = `${days}`;
+    document.getElementById("hours").innerHTML = `${hours}`;
+    document.getElementById("minutes").innerHTML = `${minutes}`;
+    document.getElementById("seconds").innerHTML = `${seconds}`;
+  }, 1000); // Update every 1 second
+})(eventDate)
+
+const generateDogsHTML = function(cards) {
+  let html = ``;
+
+  cards.forEach(el => {
+    html += `
+     <figure class="card">
+      <img src="${el.imgUrl}" alt="${el.breed}">
+      <figcaption>
+          <h2 class="dog-descr">${el.id} - ${el.breed}</h2>
+          <div>
+            <p class="dog-details">Gene:<strong>${el.gene}</strong></p>
+            <p>Age: <strong>${el.ageMonths} months</strong></p>
+          </div>
+          <p class="price">${el.price} VND</p>
+      </figcaption>
+     </figure>
+    `;
+  });
+
+  return html; 
+};
+const dogsEl = document.querySelector('.dogs-cards-js')
+dogsEl.insertAdjacentHTML('beforeend',generateDogsHTML(dogs))
+
+const generateProductsHtml = function(cards){
+  let html = ``;
+
+  cards.forEach(el => {
+    html += `
+        <figure class="card">
+          <img src="${el.imgUrl}" alt="${el.desc}">
+          <figcaption>
+            <h2 class="product-name">${el.desc}</h2>
+            <div>         
+              <p class="">Product:<strong>${el.product}</strong></p>
+              ${el.sizeGrams?`<p class="">Food Size:<strong>${el.sizeGrams} grams</strong></p>`:''}
+              <p class="price">${el.price} VND</p>                 
+            </div>
+
+            ${el.bonus? `
+            <div class="card-bonus">
+              <img src="../imgs/icons/gift.svg" alt="">
+              <p>${el.bonus.value}</p>
+            </div>`:''}
+          </figcaption>
+        </figure>
+    `
+  });
+
+  return html; 
+}
+const productsEl = document.querySelector('.product-cards-js')
+productsEl.insertAdjacentHTML('beforeend', generateProductsHtml(products))
+
+
+const generateFactsHTML= function(cards){
+  let html = ``;
+
+  cards.slice(0,3).forEach(el => {
+    html += `
+        <div class="fact-card">
+          <img src="${el.imgUrl}" alt="">
+          <div>
+            <aside>
+              <p>Pet knowledge</p>
+            </aside>
+            <h4>${el.header}</h4>
+            <p>${el.fact}</p>
+          </div>
+        </div>
+    `
+  });
+  return html; 
+}
+const factsEl = document.querySelector('.fact-cards-js')
+factsEl.insertAdjacentHTML('beforeend',generateFactsHTML(petFacts))
